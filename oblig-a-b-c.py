92a6c29be2b2c4f65912a16ca633ae314a33f88c
s344545 <102217144+s344545@users.noreply.github.com>
2022-03-23 10:49:53 +0100

    Update oblig-a-b-c.py

--- oblig-a-b-c.py
+++ oblig-a-b-c.py
@@ -7,10 +7,12 @@ Created on Wed Mar  9 15:35:52 2022
 
 ''' Del (a) '''
 
+# Opening the file and reading the data
 input_file = open('oil_raw_data.txt', 'r')
 lines = input_file.readlines()
 input_file.close()
   
+# Storing date, oil and volume in three lists 
 date= []; oil= []; volume= []
 comma= ','; space= ' ';
         
@@ -30,8 +32,9 @@ for i in range(1, 20):
           
 ''' Del (b)  '''
 
+# Removing the blank spaces
 volume_modified=[]
-for i in range(0,19):
+for i in range(0,19): 
     
   if comma not in volume[i]:
    volume_modified.append(volume[i])
@@ -41,7 +44,7 @@ for i in range(0,19):
     volume_modified.append(itemv)
                   
 
-  
+ # Changing the format to year/month/date 
 oil_modified=[]
 for k in range(0,19): 
  if comma not in oil[k]:
@@ -57,7 +60,7 @@ for d in range(0, 19):
  date_modified.append(date[d][-4:] + "/" +date[d][:2]+ "/" + date[d][-7:-5])
   
 ''' Del (c) '''
-
+# Wrtiting the list to "oil.txt", separated by comma
 output_file = open('oil.txt', 'w')
 output_file.write('date, oil, volume \n')
 for i in range(0,19): 
